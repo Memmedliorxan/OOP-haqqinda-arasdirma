6e1a942c123f17abcdd6396b5908bc218fab5ba0
Memmedliorxan <98798971+Memmedliorxan@users.noreply.github.com>
2022-02-16 22:52:17 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Chararrayfinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = { "car", "bus", "apple", "plain" };
+            char checking = 'a';
+            foreach (string item in Chararrayfinder(arr, checking))
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static string[] Chararrayfinder(string[] arr, char b)
+        {
+            string[] finially = new string[0];
+
+            foreach (string item in arr)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (item[i] == b)
+                    {
+
+                        Array.Resize(ref finially, finially.Length + 1);
+
+
+                        finially[finially.Length - 1] = item;
+                        break;
+                    }
+
+                }
+            }
+
+            return finially;
+        }
+
+
+
+        
+    }
+}
